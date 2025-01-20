6844bdd510fceb744e643eac3dab0e856d3232cf
Phi-hung Hoang <phi-hung.hoang@epita.fr>
2025-01-21 00:48:18 +0100

    project installment and initiation into mlflow

---
+++ src/features.py
@@ -0,0 +1,143 @@
+import pandas as pd
+import re
+import json
+from sklearn.preprocessing import OrdinalEncoder
+import datetime
+
+
+
+pd.options.display.max_columns = 100
+pd.options.display.max_rows = 60
+pd.options.display.max_colwidth = 100
+pd.options.display.precision = 10
+pd.options.display.width = 160
+
+def rename_columns(columns):
+    # en minuscule
+    columns = [col.lower() for col in columns]
+    # regex de remplacement
+    rgxs = [
+        (r"[°|/|']", "_"),
+        (r"²", "2"),
+        (r"[(|)]", ""),
+        (r"é|è", "e"),
+        (r"_+", "_"),
+    ]
+    # on remplace toutes les colonnes une par une
+    for rgx in rgxs:
+        columns = [re.sub(rgx[0], rgx[1], col) for col in columns]
+
+    return columns
+
+data = pd.read_csv("../data/dpe-v2-tertiaire-2.csv")
+
+data.columns = rename_columns(data.columns)
+
+id_col = 'n_dpe'
+target = 'etiquette_dpe'
+
+data.dropna(subset=target, inplace=True)
+
+columns_categorical = [
+    "periode_construction",
+    "secteur_activite",
+    "type_energie_principale_chauffage",
+    "type_energie_n_1",
+    "type_usage_energie_n_1",
+]
+
+for col in columns_categorical:
+    data[col] = data[col].fillna("non renseigné")
+
+type_energie_map = {
+    "non renseigné": "non renseigné",
+    "Électricité": "Électricité",
+    "Électricité d'origine renouvelable utilisée dans le bâtiment": "Électricité",
+    "Gaz naturel": "Gaz naturel",
+    "Butane": "GPL",
+    "Propane": "GPL",
+    "GPL": "GPL",
+    "Fioul domestique": "Fioul domestique",
+    "Réseau de Chauffage urbain": "Réseau de Chauffage urbain",
+    "Charbon": "Combustible fossile",
+    "autre combustible fossile": "Combustible fossile",
+    "Bois – Bûches": "Bois",
+    "Bois – Plaquettes forestières": "Bois",
+    "Bois – Granulés (pellets) ou briquettes": "Bois",
+    "Bois – Plaquettes d’industrie": "Bois",
+}
+
+for col in [
+    "type_energie_principale_chauffage",
+    "type_energie_n_1",
+]:
+    data[col] = data[col].apply(lambda d: type_energie_map[d])
+
+type_usage_map = {
+    "non renseigné": "non renseigné",
+    "périmètre de l'usage inconnu": "non renseigné",
+    "Chauffage": "Chauffage",
+    "Eau Chaude sanitaire": "Eau Chaude sanitaire",
+    "Eclairage": "Eclairage",
+    "Refroidissement": "Refroidissement",
+    "Ascenseur(s)": "Ascenseur(s)",
+    "auxiliaires et ventilation": "Refroidissement",
+    "Autres usages": "Autres usages",
+    "Bureautique": "Autres usages",
+    "Abonnements": "Autres usages",
+    "Production d'électricité à demeure": "Autres usages",
+}
+
+
+data["type_usage_energie_n_1"] = data["type_usage_energie_n_1"].apply(lambda d: type_usage_map[d])
+
+periode_construction_map = {
+    "avant 1948": "avant 1948",
+    "1948-1974": "1948-1974",
+    "1975-1977": "1975-1977",
+    "1978-1982": "1978-1982",
+    "1983-1988": "1983-1988",
+    "1989-2000": "1989-2000",
+    "2001-2005": "2001-2005",
+    "2006-2012": "2006-2012",
+    "2013-2021": "2013-2021",
+    "après 2021": "après 2021",
+}
+
+encoder = OrdinalEncoder()
+
+data[columns_categorical] = encoder.fit_transform(data[columns_categorical])
+for col in columns_categorical:
+    data[col] = data[col].astype(int)
+
+mappings = {}
+for i, col in enumerate(encoder.feature_names_in_):
+    mappings[col] = {int(value): category for value, category in enumerate(encoder.categories_[i])}
+
+with open("../data/categorical_mappings.json", "w", encoding="utf-8") as f:
+    json.dump(mappings, f, ensure_ascii=False, indent=4)
+
+columns_int = [
+    "version_dpe",
+    "surface_utile",
+    "conso_kwhep_m2_an",
+    "conso_e_finale_energie_n_1",
+]
+
+for col in columns_int:
+    data[col] = data[col].fillna(-1.0)
+    data[col] = data[col].astype(int)
+
+map_target = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9}
+data[target] = data[target].apply(lambda d: map_target[d])
+
+train_columns = columns_int + columns_categorical
+features = [id_col] + train_columns + [target]
+
+data = data[features].copy()
+data.reset_index(drop=True, inplace=True)
+
+output_file = f"../data/dpe_processed_{datetime.datetime.now().strftime('%Y%m%d')}.csv"
+
+data.to_csv(output_file, index=False)
+print(f"data saved to {output_file}")
---
+++ src/train.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split, GridSearchCV, KFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+
+import mlflow
+
+remote_server_uri = "http://localhost:8088"
+mlflow.set_tracking_uri(remote_server_uri)
+
+mlflow.set_experiment("experiment_01")
+
+mlflow.sklearn.autolog()
+
+input_file = "../data/dpe_processed_20250120.csv"
+data = pd.read_csv(input_file)
+
+data.sample(frac=1, random_state=42).reset_index(drop=True)
+
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+assert y.name == "etiquette_dpe"
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+X_train = X_train.drop(columns=["n_dpe"])
+id_test = list(X_test["n_dpe"])
+X_test = X_test.drop(columns=["n_dpe"])
+
+rf = RandomForestClassifier()
+
+# Define the parameter grid
+param_grid = {
+        "n_estimators": [200, 300],  # Number of trees
+        "max_depth": [10],  # Maximum depth of the trees
+        "min_samples_leaf": [1, 5],  # Maximum depth of the trees
+}
+
+# Setup GridSearchCV with k-fold cross-validation
+cv = KFold(n_splits=3, random_state=84, shuffle=True)
+
+grid_search = GridSearchCV(
+    estimator=rf, param_grid=param_grid, cv=cv, scoring="accuracy", verbose=1
+)
+
+# Fit the model
+grid_search.fit(X_train, y_train)
+
+# Best parameters and best score
+print(f"Best parameters: {grid_search.best_params_}")
+print(f"Best cross-validation score: {grid_search.best_score_}")
+print(f"Best model: {grid_search.best_estimator_}")
+
+# Evaluate on the test set
+yhat = grid_search.predict(X_test)
+print(classification_report(y_test, yhat))
+
+probabilities = grid_search.predict_proba(X_test)
+
+predictions = pd.DataFrame()
+predictions.index = id_test
+predictions["prob"] = np.max(probabilities, axis=1)
+predictions["yhat"] = yhat
+predictions["y"] = y_test.values
+print(predictions.head())
+
+# feature importance
+feature_importances = grid_search.best_estimator_.feature_importances_
+feature_names = X_train.columns
+
+# Create a dictionary mapping feature names to their importance
+importance_dict = dict(zip(feature_names, feature_importances))
+importance_dict = dict(
+    sorted(importance_dict.items(), key=lambda item: item[1], reverse=True)
+)
+
+print(importance_dict)
